c447186d53655e0f951458838a1bc803f5facd81
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-16 12:46:31 +0200

    Fix the shadow

--- components/categories/CategoryGridTile.js
+++ components/categories/CategoryGridTile.js
@@ -14,7 +14,7 @@ const CategoryGridTile = (props) => {
     TouchableCmp = TouchableNativeFeedback
   }
   return (
-    <View style={styles.gridElement}>
+    <View style={styles.gridItem}>
       <TouchableCmp style={{ flex: 1 }} onPress={props.onSelect}>
         <View
           style={{ ...styles.container, ...{ backgroundColor: props.color } }}
@@ -31,12 +31,16 @@ const CategoryGridTile = (props) => {
 export default CategoryGridTile
 
 const styles = StyleSheet.create({
-  gridElement: {
+  gridItem: {
     flex: 1,
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: 'hidden',
+    overflow:
+      Platform.OS === 'android' && Platform.Version >= 21
+        ? 'hidden'
+        : 'visible',
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -45,7 +49,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
