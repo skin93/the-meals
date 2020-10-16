0a32880dd69120c751cff3bf757ca81cd301bdd9
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-16 10:05:54 +0200

    Add MEALS dummy data & load them for categories

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,32 +1,31 @@
 import React from 'react'
 import { StyleSheet, Text, Button, View } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 
-import Colors from '../constants/Colors'
-
-import { CATEGORIES } from '../data/dummy-data'
+import { CATEGORIES, MEALS } from '../data/dummy-data'
 
 const CategoryMealsScreen = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    )
+  }
   const catId = props.navigation.getParam('categoryId')
+
+  const displayedMeals = MEALS.filter((meal) =>
+    meal.categoryIds.indexOf(catId >= 0)
+  )
 
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catId)
 
   return (
     <View style={styles.screen}>
-      <Text>The Category Meals Screen</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title='Go to Details'
-        onPress={() => {
-          props.navigation.navigate({
-            routeName: 'MealDetail',
-          })
-        }}
-      />
-      <Button
-        title='Go Back'
-        onPress={() => {
-          props.navigation.pop()
-        }}
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
       />
     </View>
   )
